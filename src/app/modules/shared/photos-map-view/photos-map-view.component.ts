8d3e6ed217bebc62de2b6b43a1f8ca36bb1bddd2
Vitaly Dudarau <vitalydudarev@gmail.com>
2023-05-02 22:12:02 +0300

    Disabled obsolete libraries.

--- src/app/modules/shared/photos-map-view/photos-map-view.component.ts
+++ src/app/modules/shared/photos-map-view/photos-map-view.component.ts
@@ -1,42 +1,48 @@
-import { Component, ElementRef, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
-import { AgmInfoWindow } from '@agm/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  ViewChild,
+} from "@angular/core";
+// import { AgmInfoWindow } from '@agm/core';
 
-import { MarkerWrapper } from 'src/app/core/models/marker-wrapper.model';
-import { Photo } from 'src/app/core/models/photo.model';
-import { GoogleMapHelper } from 'src/app/core/helpers/google-map.helper';
+import { MarkerWrapper } from "src/app/core/models/marker-wrapper.model";
+import { Photo } from "src/app/core/models/photo.model";
+import { GoogleMapHelper } from "src/app/core/helpers/google-map.helper";
 
 @Component({
-    selector: 'app-photos-map-view',
-    templateUrl: './photos-map-view.component.html'
+  selector: "app-photos-map-view",
+  templateUrl: "./photos-map-view.component.html",
 })
 export class PhotosMapViewComponent implements OnChanges {
-    @Input() photos: Photo[];
-    @ViewChild('map', {read: ElementRef, static: true}) map: ElementRef;
+  @Input() photos: Photo[];
+  @ViewChild("map", { read: ElementRef, static: true }) map: ElementRef;
 
-    markers: MarkerWrapper[] = [];
-    zoom: number;
-    center: { lat: number, lng: number } = { lat: 0, lng: 0 };
+  markers: MarkerWrapper[] = [];
+  zoom: number;
+  center: { lat: number; lng: number } = { lat: 0, lng: 0 };
 
-    private infoWindowOpened: AgmInfoWindow;
-    private previousInfoWindow: AgmInfoWindow;
+  // private infoWindowOpened: AgmInfoWindow;
+  // private previousInfoWindow: AgmInfoWindow;
 
-    constructor() {
-    }
+  constructor() {}
 
-    ngOnChanges(changes: SimpleChanges): void {
-        this.previousInfoWindow = null;
-        this.infoWindowOpened = null;
+  ngOnChanges(changes: SimpleChanges): void {
+    // this.previousInfoWindow = null;
+    // this.infoWindowOpened = null;
 
-        const markers = this.createMarkers();
+    const markers = this.createMarkers();
 
-        const map = document.getElementsByClassName('agm-map-container-inner sebm-google-map-container-inner')[0];
+    // const map = document.getElementsByClassName('agm-map-container-inner sebm-google-map-container-inner')[0];
 
-        this.zoom = GoogleMapHelper.getBoundsZoomLevel(markers, { height: map.clientHeight, width: map.clientWidth });
-        this.center = GoogleMapHelper.getCenter(markers);
-        this.markers = markers;
-    }
+    // this.zoom = GoogleMapHelper.getBoundsZoomLevel(markers, { height: map.clientHeight, width: map.clientWidth });
+    this.center = GoogleMapHelper.getCenter(markers);
+    this.markers = markers;
+  }
 
-    onMarkerClicked(infoWindow: AgmInfoWindow) {
+  /*onMarkerClicked(infoWindow: AgmInfoWindow) {
         if (!this.previousInfoWindow) {
             this.previousInfoWindow = infoWindow;
         } else {
@@ -51,37 +57,47 @@ export class PhotosMapViewComponent implements OnChanges {
         if (this.previousInfoWindow) {
             this.previousInfoWindow.close();
         }
+    }*/
+
+  private createMarkers(): MarkerWrapper[] {
+    const markers: MarkerWrapper[] = [];
+
+    for (const photo of this.photos) {
+      if (photo.latitude && photo.longitude) {
+        const title = photo.fileName;
+        const marker = this.createMarker(
+          title,
+          photo.latitude,
+          photo.longitude,
+          photo.thumbnailSmallUrl
+        );
+
+        markers.push(marker);
+      }
     }
 
-    private createMarkers(): MarkerWrapper[] {
-        const markers: MarkerWrapper[] = [];
+    return markers;
+  }
 
-        for (const photo of this.photos) {
-            if (photo.latitude && photo.longitude) {
-                const title = photo.fileName;
-                const marker = this.createMarker(title, photo.latitude, photo.longitude, photo.thumbnailSmallUrl);
+  private createMarker(
+    title: string,
+    latitude: number,
+    longitude: number,
+    thumbnailUrl: string
+  ): MarkerWrapper {
+    const icon = {
+      url: thumbnailUrl,
+      scaledSize: { width: 64, height: 64 },
+      origin: { x: 0, y: 0 },
+      anchor: { x: 0, y: 0 },
+    };
 
-                markers.push(marker);
-            }
-        }
-
-        return markers;
-    }
-
-    private createMarker(title: string, latitude: number, longitude: number, thumbnailUrl: string): MarkerWrapper {
-        const icon = {
-            url: thumbnailUrl,
-            scaledSize: { width: 64, height: 64 },
-            origin: { x: 0, y: 0 },
-            anchor: { x: 0, y: 0 }
-        };
-
-        return {
-            latitude,
-            longitude,
-            title,
-            icon,
-            previewImageUrl: thumbnailUrl
-        } as MarkerWrapper;
-    }
+    return {
+      latitude,
+      longitude,
+      title,
+      icon,
+      previewImageUrl: thumbnailUrl,
+    } as MarkerWrapper;
+  }
 }
