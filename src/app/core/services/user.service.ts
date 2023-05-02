cbc1fc82d3d922059f55bfaf8df0647ee20c3dba
Vitaly Dudarau <vitalydudarev@gmail.com>
2023-05-02 18:21:40 +0300

    Improvements.

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,23 +1,34 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
-import { User } from '../models/user.model';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
+import { User } from "../models/user.model";
 
 @Injectable()
 export class UserService {
-
   private url: string = `${environment.photoMapApiUrl}/users`;
 
-  constructor(private _httpClient: HttpClient) {
-  }
+  constructor(private _httpClient: HttpClient) {}
 
   addUser(id: number, name: string): Observable<any> {
     return this._httpClient.post<any>(`${this.url}`, { id: id, name: name });
   }
 
-  updateUser(id: number, name: string, yandexDiskToken: string, yandexDiskTokenExpiresIn: number, dropboxToken: string, dropboxTokenExpiresIn: number): Observable<any> {
-    return this._httpClient.patch<any>(`${this.url}/${id}`, { name: name, yandexDiskToken: yandexDiskToken, yandexDiskTokenExpiresIn: yandexDiskTokenExpiresIn, dropboxToken: dropboxToken, dropboxTokenExpiresIn: dropboxTokenExpiresIn });
+  updateUser(
+    id: number,
+    name: string,
+    yandexDiskToken: string,
+    yandexDiskTokenExpiresIn: number,
+    dropboxToken: string,
+    dropboxTokenExpiresIn: number
+  ): Observable<any> {
+    return this._httpClient.patch<any>(`${this.url}/${id}`, {
+      name: name,
+      yandexDiskToken: yandexDiskToken,
+      yandexDiskTokenExpiresIn: yandexDiskTokenExpiresIn,
+      dropboxToken: dropboxToken,
+      dropboxTokenExpiresIn: dropboxTokenExpiresIn,
+    });
   }
 
   getUser(id: number): Observable<User> {
@@ -25,6 +36,6 @@ export class UserService {
   }
 
   getUserImages(): Observable<string[]> {
-    return  this._httpClient.get<string[]>(`${this.url}/images`);
+    return this._httpClient.get<string[]>(`${this.url}/images`);
   }
 }
